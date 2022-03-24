f9ce6b108b515ee652ad59554e50e5a375c79f27
Benjamin van Seggelen <benjaminvanseggelen@hotmail.com>
2022-03-24 14:06:03 +0100

    Sub fwbr, formatting

--- calculate_fwbr.py
+++ calculate_fwbr.py
@@ -1,10 +1,30 @@
 import csv
 from math import log10 as log
 
+
 def calculate_fwbr(CBO, DIT, WMC, RFC, LCOM, NOC):
-    return -1 * (8.753 * log(CBO + 1) + 2.505 * log(DIT + 1) 
-    - 1.922 * log(WMC + 1) + 0.892 * log(RFC + 1) 
-    - 0.399 * log(LCOM + 1) - 1.080 * log(NOC + 1))
+    return -1 * (8.753 * log(CBO + 1) + 2.505 * log(DIT + 1)
+                 - 1.922 * log(WMC + 1) + 0.892 * log(RFC + 1)
+                 - 0.399 * log(LCOM + 1) - 1.080 * log(NOC + 1))
+
+
+def calculate_fwbr_s(CBO, DIT, WMC, RFC, LCOM, NOC):
+    return -1 * (5.810 * log(CBO + 1) + 3.112 * log(DIT + 1)
+                 + 0.067 * log(WMC + 1) - 1.012 * log(RFC + 1)
+                 - 0.041 * log(LCOM + 1) - 0.132 * log(NOC + 1))
+
+
+def calculate_fwbr_n(CBO, DIT, WMC, RFC, LCOM, NOC):
+    return -1 * (9.023 * log(CBO + 1) + 3.945 * log(DIT + 1)
+                 - 0.678 * log(WMC + 1) - 0.826 * log(RFC + 1)
+                 - 0.378 * log(LCOM + 1))
+
+
+def calculate_fwbr_l(CBO, DIT, WMC, RFC, LCOM, NOC):
+    return -1 * (8.151 * log(CBO + 1) + 1.431 * log(DIT + 1)
+                 - 2.788 * log(WMC + 1) + 2.501 * log(RFC + 1)
+                 - 0.191 * log(LCOM + 1) - 1.242 * log(NOC + 1))
+
 
 with open('versions.csv') as f:
     csv_reader = csv.DictReader(f)
@@ -16,11 +36,15 @@ with open('versions.csv') as f:
 
         print(f'\n\nVersion: {version}')
 
+        # First count the number of classes
+        with open(f'./ck-metrics/ck-metrics-{version}.csv') as ck_f:
+            number_of_classes = sum(1 for class_line in ck_f) - 1
+
         with open(f'./ck-metrics/ck-metrics-{version}.csv') as ck_f:
             ck_csv_reader = csv.DictReader(ck_f)
-            
+
             with open(f'./fwbr-metrics/fwbr-metrics-{version}.csv', 'w') as o:
-                o.write('ClassName, fwbr\n')
+                o.write('ClassName, fwbr, fwbr_sub_type, fwbr_sub\n')
 
                 for ck_row in ck_csv_reader:
                     class_name = ck_row['ClassName']
@@ -30,5 +54,16 @@ with open('versions.csv') as f:
                     RFC = int(ck_row['RFC'])
                     LCOM = int(ck_row['LCOM'])
                     NOC = int(ck_row['NOC'])
-                    
-                    o.write(f'{class_name}, {calculate_fwbr(CBO, DIT, WMC, RFC, LCOM, NOC)}\n')
+
+                    o.write(
+                        f'{class_name}, {calculate_fwbr(CBO, DIT, WMC, RFC, LCOM, NOC)}, ')
+
+                    if number_of_classes <= 500:
+                        o.write(
+                            f's, {calculate_fwbr_s(CBO, DIT, WMC, RFC, LCOM, NOC)}\n')
+                    elif number_of_classes <= 1400:
+                        o.write(
+                            f'n, {calculate_fwbr_n(CBO, DIT, WMC, RFC, LCOM, NOC)}\n')
+                    else:
+                        o.write(
+                            f'l, {calculate_fwbr_l(CBO, DIT, WMC, RFC, LCOM, NOC)}\n')
--- run_metrics.py
+++ run_metrics.py
@@ -9,12 +9,15 @@ with open('versions.csv') as f:
         lib_directory = row['libdirectory']
         jar_path = row['jarpath']
 
-        print(f'\n\nVersion: {version}\n Library directoy: {lib_directory}\n JAR path: {jar_path}')
+        print(
+            f'\n\nVersion: {version}\n Library directoy: {lib_directory}\n JAR path: {jar_path}')
 
         if lib_directory:
             # To skip 5.5.13.3
             with open(f'./ck-metrics/ck-metrics-{version}.csv', 'w') as o:
-                subprocess.run(['java', '-jar', 'ckjm_adapted.jar', '.'+jar_path, '.'+lib_directory], stdout=o)
-        
+                subprocess.run(['java', '-jar', 'ckjm_adapted.jar',
+                               '.'+jar_path, '.'+lib_directory], stdout=o)
+
         with open(f'./le-metrics/le-metrics-{version}.csv', 'w') as o:
-            subprocess.run(['java', '-jar', 'layer_extractor.jar', '.'+jar_path], stdout=o)
+            subprocess.run(
+                ['java', '-jar', 'layer_extractor.jar', '.'+jar_path], stdout=o)
